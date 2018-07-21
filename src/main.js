6049c438bfcd5b416cfa26b538fd019a88c130c0
weicq <1326774414@qq.com>
2018-07-21 19:42:28 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.min.css'
 
+import './plugins/axios'
+
 NProgress.inc(0.2)
 NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
 
